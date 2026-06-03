aa862d5ba94c591ab3c07827f2d89d1e5df24e88
erastushs <erastushs@gmail.com>
2026-06-03 11:51:33 +0900

    add vercel analytics & speedInsights

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://luxyhub.vercel.app/sitemap.xml',
+    sitemap: 'https://luxyhub.space/sitemap.xml',
   }
 }
