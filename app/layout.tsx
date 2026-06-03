fe00eff115a59a481a039eb281d50224f26c7d63
erastushs <erastushs@gmail.com>
2026-06-03 10:37:10 +0900

    add vercel analytics & speedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'sonner'
 import CustomCursor from './components/CustomCursor'
+import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -99,6 +101,8 @@ export default function RootLayout({
         <CustomCursor />
         {children}
         <Toaster position="bottom-right" richColors />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
